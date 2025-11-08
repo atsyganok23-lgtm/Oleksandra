ec855a841a22ba8734e37ffc253738be181ec4a6
Oleksandra <atsyganok23@gmail.com>
2025-11-08 17:45:57 +0200

    homework lesson6, 01.11.2025

---
+++ lesson7/directory1/module1.py
@@ -0,0 +1,4 @@
+def make_country (name, capital):
+    dictionary = {"name":name, "capital":capital}
+    print (f'Country:{dictionary["name"]}, Capital:{dictionary["capital"]}')
+
---
+++ lesson7/directory1/module2.py
@@ -0,0 +1,3 @@
+from module1 import make_country
+message = make_country("Poland", "Warsaw")
+print(message)
